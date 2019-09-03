5217ecd1dbb95525e12a1025bddea1c6b7d90d6f
ThomasDenneulin <thomas.denneulin@devinci.fr>
2019-09-03 12:22:14 +0200

    first route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Http\Resources\PlayerResource;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,4 +15,9 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/Players', function() {
+    return new PlayerResource(Players::all());
 });
